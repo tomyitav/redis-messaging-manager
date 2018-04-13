b2f157f8a58a778dc8f5cf34fcf383086fc63c41
Tom <tomyitav@gmail.com>
2018-04-14 02:04:21 +0300

    feat: added missing files
    added missing files

--- src/redis-messenger.ts
+++ src/redis-messenger.ts
@@ -64,7 +64,7 @@ export class PubsubManager {
         let delay = Math.min(100 + times * 2, 2000)
         return delay
       },
-      reconnectOnError: err => true
+      reconnectOnError: () => true
     }
   }
 }
